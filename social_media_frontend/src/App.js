6debeafbf76236034bf8b2452cc7727878b19fe0
Johirul Alam <johirulalam365@gmail.com>
2023-04-08 20:01:31 +0600

    create components

--- social_media_frontend/src/App.js
+++ social_media_frontend/src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes , Route } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './container/Home';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
+    <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/*' element={<Home/>}/>
+    </Routes>
   );
 }
 
